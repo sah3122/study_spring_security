fa710d5efdc6a75e77aa12eb34218d41a7ea3755
dongchul <sah3122@naver.com>
2019-08-27 14:49:42 +0900

    spring security password encoder setting

--- src/main/java/me/study/demospringsecurityform/DemoSpringSecurityFormApplication.java
+++ src/main/java/me/study/demospringsecurityform/DemoSpringSecurityFormApplication.java
@@ -2,12 +2,21 @@ package me.study.demospringsecurityform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoSpringSecurityFormApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringSecurityFormApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); //spring security 5 에서 제공하는 방식. encoding 방식은 커스텀 할 수 있음.
     }
 
 }
