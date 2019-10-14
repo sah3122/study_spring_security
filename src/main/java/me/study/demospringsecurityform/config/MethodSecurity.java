58caf29d013dca2240101e29abc51e11c6cc0b85
dongchul <sah3122@naver.com>
2019-10-14 23:56:52 +0900

    ADD Method Security

---
+++ src/main/java/me/study/demospringsecurityform/config/MethodSecurity.java
@@ -0,0 +1,25 @@
+package me.study.demospringsecurityform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.vote.RoleHierarchyVoter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
+public class MethodSecurity extends GlobalMethodSecurityConfiguration {
+
+    // WEB Security와는 별개로 Method Security 설정은 따로 해주어야 한다.
+    // Method 용 계층형 구조 설정
+    @Override
+    protected AccessDecisionManager accessDecisionManager() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        AffirmativeBased accessDecisionManager = (AffirmativeBased) super.accessDecisionManager();
+        accessDecisionManager.getDecisionVoters().add(new RoleHierarchyVoter(roleHierarchy));
+        return accessDecisionManager;
+    }
+}
